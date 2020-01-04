f498e379918b3ba142c8c8f30d6032e1ca56c584
dofoyo <dofoyo@sina.cn>
2020-01-04 11:30:34 +0800

    optimized

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-del d:\workspace\joojoo\src\main\webapp\index.html
-rd d:\workspace\joojoo\src\main\webapp\static /s /q
+del d:\dev\joojoo\src\main\webapp\index.html
+rd d:\dev\joojoo\src\main\webapp\static /s /q
 
-xcopy d:\workspace\joojoo-web\dist\*.* d:\workspace\joojoo\src\main\webapp\ /e
+xcopy d:\dev\joojoo-web\dist\*.* d:\dev\joojoo\src\main\webapp\ /e
